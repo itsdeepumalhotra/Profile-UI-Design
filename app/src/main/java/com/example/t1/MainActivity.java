27b328234fbcee0c4841700f5d1b8c20d5478323
itsdeepumalhotra <deepanshumalhotra2017@gmail.com>
2022-01-08 18:11:19 +0530

    Profile Design

---
+++ app/src/main/java/com/example/t1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Entered Activity",Toast.LENGTH_LONG).show();
+
+    }
+}
